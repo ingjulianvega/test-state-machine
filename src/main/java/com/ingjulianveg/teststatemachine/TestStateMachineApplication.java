2f9d4d564477030d726b631e025105ee54c35843
Julian Camilo Vega Rocha <julian.vega@globant.com>
2022-03-24 11:37:31 -0500

    ✨ Initial commit

---
+++ src/main/java/com/ingjulianveg/teststatemachine/TestStateMachineApplication.java
@@ -0,0 +1,13 @@
+package com.ingjulianveg.teststatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStateMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStateMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingjulianveg/teststatemachine/TestStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingjulianveg.teststatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
